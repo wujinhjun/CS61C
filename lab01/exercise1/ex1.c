955e482725127a00674da34fbb2c8164faf37a0f
p_kunwwei <p_kunwwei@tencent.com>
2023-07-17 23:40:55 +0800

    feat: computer nucleotide occurrences

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -29,5 +29,9 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    dna_seq->A_count = num_occurrences(dna_seq->sequence, 'A');
+    dna_seq->C_count = num_occurrences(dna_seq->sequence, 'C');
+    dna_seq->G_count = num_occurrences(dna_seq->sequence, 'G');
+    dna_seq->T_count = num_occurrences(dna_seq->sequence, 'T');
     return;
 }
