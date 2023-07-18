d7f33d2ccd6bf7b035a9ed0667f0b7547f9705ac
p_kunwwei <p_kunwwei@tencent.com>
2023-07-18 15:42:56 +0800

    feat: exercise4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *fast_pointer = head;
+    node *slow_pointer = head;
+    
+    while (fast_pointer != NULL && fast_pointer->next != NULL && fast_pointer->next->next != NULL)
+    {
+        slow_pointer = slow_pointer->next;
+        fast_pointer = fast_pointer->next->next;
+
+        if (slow_pointer == fast_pointer) {
+            return 1;
+        }
+    }
+    return 0;
 }
