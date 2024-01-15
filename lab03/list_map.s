9eb2c5ea4bfd0736b914ce0cb1aa5faf11301b60
wujinhjun <wujinhjun@outlook.com>
2024-01-15 15:56:30 +0800

    docs: update the comment

--- lab03/list_map.s
+++ lab03/list_map.s
@@ -105,24 +105,48 @@ decrement:
 # You don't need to understand these, but reading them may be useful
 
 create_default_list:
+    # sp: stack pointer
+    # sp-12, 12 bytes space (3 words)
     addi sp, sp, -12
+
+    # ra 0 sp
     sw ra, 0(sp)
+    # s0 4 sp
     sw s0, 4(sp)
+    # s1 8 sp
     sw s1, 8(sp)
+
+    # load immediate
     li s0, 0            # Pointer to the last node we handled
     li s1, 0            # Number of nodes handled
 loop:                   # do...
+    # a0 = 8
     li a0, 8
+    # revoke the malloc function
+    # the parameter is a0
     jal ra, malloc      #     Allocate memory for the next node
+    # a0 is a node
+
+    # s1 = a0.slice(0, 4)
     sw s1, 0(a0)        #     node->value = i
+    # s2 = a0.skuce(4, 8)
     sw s0, 4(a0)        #     node->next = last
+
+    # s0 = a0
     add s0, a0, x0      #     last = node
+    # number of nodes handled + 1
     addi s1, s1, 1      #     i++
+    # t0 = 10
     addi t0, x0, 10
     bne s1, t0, loop    # ... while i!= 10
+    # ra = sp[0]
     lw ra, 0(sp)
+    # s0 = sp[4]
     lw s0, 4(sp)
+    # s1 = sp[8]
     lw s1, 8(sp)
+
+    # recover the stack pointer
     addi sp, sp, 12
     jr ra
 
@@ -130,6 +154,7 @@ print_list:
     bne a0, x0, print_me_and_recurse
     jr ra               # Nothing to print
 print_me_and_recurse:
+    # t0 = a0
     add t0, a0, x0      # t0 gets current node address
     lw a1, 0(t0)        # a1 gets value in current node
     addi a0, x0, 1      # Prepare for print integer ecall
@@ -147,7 +172,9 @@ print_newline:
     jr ra
 
 malloc:
+    # a1 = a0 + 0   
     addi a1, a0, 0
+    # a0 = 9
     addi a0, x0, 9
     ecall
     jr ra
